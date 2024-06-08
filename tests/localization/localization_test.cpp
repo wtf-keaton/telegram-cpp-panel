f96cfdbe9b4b32eb53761bc6e0bf0cfee0371ecc
withoutforget <89633210+withoutforget@users.noreply.github.com>
2024-06-09 02:25:24 +0300

    Updated formatting for localization_test.cpp

--- tests/localization/localization_test.cpp
+++ tests/localization/localization_test.cpp
@@ -6,7 +6,7 @@ constexpr auto ENABLE_EXCEPTION_RESULT = true;
 #define LOC_TEST
 
 #ifdef LOC_TEST
-#define MAIN main 
+#define MAIN main
 #else
 #define MAIN localization_test_main
 #endif
@@ -14,37 +14,36 @@ constexpr auto ENABLE_EXCEPTION_RESULT = true;
 #include "localization.h"
 
 void init_russian_localization() {
-    using enum sh::localization_t;
-    auto& localization = sh::localization::instance();
-    localization[ru].add("welcome_text", "Привет мир!");
-    localization[ru].add("bye_text", "Пока мир!");
-    localization[ru].add("random", "Рандомный текст, Например, 1234.");
-    if constexpr (ENABLE_EXCEPTION_RESULT)
-        localization[ru].add("random", "rand");
+  using enum sh::localization_t;
+  auto& localization = sh::localization::instance();
+  localization[ru].add("welcome_text", "Привет мир!");
+  localization[ru].add("bye_text", "Пока мир!");
+  localization[ru].add("random", "Рандомный текст, Например, 1234.");
+  if constexpr (ENABLE_EXCEPTION_RESULT) localization[ru].add("random", "rand");
 }
 
 void init_english_localization() {
-    using enum sh::localization_t;
-    auto& localization = sh::localization::instance();
-    localization[en].add("welcome_text", "Hello world!");
-    localization[en].add("bye_text", "Bye world!");
-    localization[en].add("random", "Here's nothing for you.");
+  using enum sh::localization_t;
+  auto& localization = sh::localization::instance();
+  localization[en].add("welcome_text", "Hello world!");
+  localization[en].add("bye_text", "Bye world!");
+  localization[en].add("random", "Here's nothing for you.");
 }
 
 int MAIN() {
-    init_english_localization();
-    init_russian_localization();
-    std::ofstream file(OUTPUT_FILENAME);
-    using enum sh::localization_t;
-    for (auto&& loc : {ru, en}) {
-        auto& local = sh::localization::instance()[loc];
-        for (auto&& key : {"welcome_text", "bye_text", "random"}) {
-            if (auto&& str = local[key]; !str.empty())
-                file << str << std::endl;
-            else
-                file << "key = '" << key << "' does not have value" << std::endl; 
-        }
+  init_english_localization();
+  init_russian_localization();
+  std::ofstream file(OUTPUT_FILENAME);
+  using enum sh::localization_t;
+  for (auto&& loc : {ru, en}) {
+    auto& local = sh::localization::instance()[loc];
+    for (auto&& key : {"welcome_text", "bye_text", "random"}) {
+      if (auto&& str = local[key]; !str.empty())
+        file << str << std::endl;
+      else
+        file << "key = '" << key << "' does not have value" << std::endl;
     }
-    file.close();
-    return 0;
+  }
+  file.close();
+  return 0;
 }
