ed5f4c2f8bead47428dcfbedabf03eaae4140268
withoutforget <89633210+withoutforget@users.noreply.github.com>
2024-06-09 18:31:47 +0300

    Added json compability for localization
    * Added methods to\from_json to class c_locale.
    * Added parsing config filename from command line arguments using --config
    * Imported getoptw.h library from withoutforget/getoptw
    * Moved namespace details from localization to details/format

--- telegram-service/localization/localization.h
+++ telegram-service/localization/localization.h
@@ -19,109 +19,87 @@
 #include <utility>  // forward
 
 #include "../hash/fnv1a.h"
+#include "../json/json.h"
+#include "details/format.h"
 
-namespace sh
-{
-	namespace details
-	{
-		template <typename... _ts>
-		constexpr std::string format( _ts&&... ts )
-		{
-			std::stringstream ss;
-			( ss << ... << std::forward<_ts>( ts ) );
-			return ss.str( );
-		}
-		constexpr std::string source_location_format( std::source_location const& lc )
-		{
-			return format( "filename'", lc.file_name( ), "' in function='",
-						   lc.function_name( ), "' at line ", lc.line( ), " column ",
-						   lc.column( ) );
-		}
-	}  // namespace details
+namespace sh {
+enum e_localization : size_t {
+  _unknown = 0,
+  _ru,
+  _en,
+  _cn,
+  _count,
+};
 
-	enum e_localization : size_t
-	{
-		_unknown = 0,
-		_ru,
-		_en,
-		_cn,
-		_count,
-	};
+class c_locale final {
+  std::map<hash_t, std::string> m_data;
 
-	class c_locale final
-	{
-		std::map<hash_t, std::string> m_data;
+ public:
+  template <typename _container>
+  void from_container(_container&& cont) {
+    for (auto&& [k, v] : cont) add(k, v);
+  }
 
-	public:
-		template <typename _container>
-		void from_container( _container&& cont )
-		{
-			for ( auto&& [k, v] : cont ) add( k, v );
-		}
+  void to_json(nlohmann::json& json) { json = m_data; }
 
-		void add( hash_t synonim, const std::string& value )
-		{
-			if ( m_data.contains( synonim ) )
-			{
-				/* @todo: Here we need logging for this exception */
-				throw std::runtime_error{ details::format(
-					"in [",
-					details::source_location_format( std::source_location::current( ) ),
-					"] cannot add second value as value='", value, "' for synonym='",
-					synonim, '\'' ) };
-			}
+  void from_json(nlohmann::json& json) {
+    m_data = json.get<std::map<hash_t, std::string>>();
+  }
 
-			m_data.emplace( synonim, value );
-		}
+  void add(hash_t synonim, const std::string& value) {
+    if (m_data.contains(synonim)) {
+      /* @todo: Here we need logging for this exception */
+      throw std::runtime_error{details::format(
+          "in [",
+          details::source_location_format(std::source_location::current()),
+          "] cannot add second value as value='", value, "' for synonym='",
+          synonim, '\'')};
+    }
 
-		std::string get( hash_t const key )
-		{
-			for ( auto&& data : m_data )
-			{
-				if ( data.first != key )
-					continue;
+    m_data.emplace(synonim, value);
+  }
 
-				return data.second;
-			}
+  std::string get(hash_t const key) {
+    for (auto&& data : m_data) {
+      if (data.first != key) continue;
 
-			return "failed to find localization";
-		}
+      return data.second;
+    }
 
-		std::string_view operator[]( hash_t const s ) const
-		{
-			auto const it = m_data.find( s );
-			if ( it == m_data.end( ) ) return {};
-			return std::string_view{ it->second };
-		}
+    return "failed to find localization";
+  }
 
-		auto begin( ) noexcept { return m_data.begin( ); }
-		auto end( ) noexcept { return m_data.end( ); }
-		auto cbegin( ) const noexcept { return m_data.cbegin( ); }
-		auto cend( ) const noexcept { return m_data.cend( ); }
-		auto rbegin( ) noexcept { return m_data.rbegin( ); }
-		auto rend( ) noexcept { return m_data.rend( ); }
-		auto crbegin( ) const noexcept { return m_data.crbegin( ); }
-		auto crend( ) const noexcept { return m_data.crend( ); }
-		bool empty( ) const noexcept { return m_data.empty( ); }
-		void clear( ) noexcept { return m_data.clear( ); }
-	};
+  std::string_view operator[](hash_t const s) const {
+    auto const it = m_data.find(s);
+    if (it == m_data.end()) return {};
+    return std::string_view{it->second};
+  }
 
-	class c_localization final
-	{
-		std::array<c_locale, _count> m_locals{};
-		c_localization( ) = default;
+  auto begin() noexcept { return m_data.begin(); }
+  auto end() noexcept { return m_data.end(); }
+  auto cbegin() const noexcept { return m_data.cbegin(); }
+  auto cend() const noexcept { return m_data.cend(); }
+  auto rbegin() noexcept { return m_data.rbegin(); }
+  auto rend() noexcept { return m_data.rend(); }
+  auto crbegin() const noexcept { return m_data.crbegin(); }
+  auto crend() const noexcept { return m_data.crend(); }
+  bool empty() const noexcept { return m_data.empty(); }
+  void clear() noexcept { return m_data.clear(); }
+};
 
-	public:
-		static c_localization& instance( )
-		{
-			static c_localization loc;
-			return loc;
-		}
+class c_localization final {
+  std::array<c_locale, _count> m_locals{};
+  c_localization() = default;
 
-		c_locale& operator[]( e_localization const lang )&
-		{
-			return m_locals.at( lang );
-		}
-	};
+ public:
+  static c_localization& instance() {
+    static c_localization loc;
+    return loc;
+  }
+
+  c_locale& operator[](e_localization const lang) & {
+    return m_locals.at(lang);
+  }
+};
 
 }  // namespace sh
